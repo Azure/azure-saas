720b2ed948f3d02aa968c17f8ced13b4332d9326
Nick Pinheiro <nickpinheiro@outlook.com>
2021-06-23 15:59:57 -0400

    Added entities to Catalog database.

---
+++ src/Saas.Catalog/Saas.Catalag.Sql/dbo/Tables/Product.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+    [Id] INT NOT NULL,
+	[Name] NVARCHAR(250) NOT NULL,  
+	[Tier] NVARCHAR(50) NULL, 
+	[Description] NTEXT NOT NULL, 
+	[ListPrice] MONEY NOT NULL, 
+    [BlobName] NVARCHAR(250) NOT NULL, 
+    [Created] DATETIME NOT NULL DEFAULT (getdate()), 
+	[DownloadCount] INT NOT NULL DEFAULT 0,  
+
+    
+    [Icon] NVARCHAR(250) NULL, 
+    PRIMARY KEY ([Id])
+)
