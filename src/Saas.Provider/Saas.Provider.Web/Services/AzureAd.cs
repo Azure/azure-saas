db4043ae04e6da816cac3c491c79dc7bc9f209ff
Nick Pinheiro <nickpinheiro@outlook.com>
2021-08-10 15:20:13 -0400

    Updated MSAL service.

--- src/Saas.Provider/Saas.Provider.Web/Services/AzureAd.cs
+++ src/Saas.Provider/Saas.Provider.Web/Services/AzureAd.cs
@@ -10,32 +10,19 @@ namespace Saas.Provider.Web.Services
 
         private const string AuthorityFormat = "https://login.microsoftonline.com/{0}/v2.0";
 
-        const string MSATenantId = "f4a840a3-b301-49d7-81dc-d598681eba1d";
-        public static string clientId = "87e0a9e3-1faf-4cb3-abf6-6d9531de0658";
-        public static string clientSecret = "k78~odcN_6TbVh5D~19_8Qkj~87trteArL";
+        const string MSATenantId = "";
+        public static string clientId = "";
+        public static string clientSecret = "";
 
-        private const string MAIdentityScope = "https://mappz.onmicrosoft.com/ModernAppz.Api.Identity/.default";
-        private const string MAOnboardingScope = "https://mappz.onmicrosoft.com/ModernAppz.Api.Onboarding/.default";
+        private const string OnboardingScope = "";
 
         public AzureAd(AppSettings appSettings)
         {
             _appSettings = appSettings;
         }
 
-        public async Task<AuthenticationResult> GetAccessTokenAsync(string service)
+        public async Task<AuthenticationResult> GetAccessTokenAsync()
         {
-            string scope = null;
-
-            switch (service)
-            {
-                case "onboarding":
-                    scope = MAOnboardingScope;
-                    break;
-                case "identity":
-                    scope = MAIdentityScope;
-                    break;
-            }
-
             // Get a token for the Microsoft Graph. If this line throws an exception for any reason, we'll just let the exception be returned as a 500 response
             // to the caller, and show a generic error message to the user.
             IConfidentialClientApplication daemonClient;
@@ -45,7 +32,7 @@ namespace Saas.Provider.Web.Services
                 .WithClientSecret(clientSecret)
                 .Build();
 
-            AuthenticationResult authResult = await daemonClient.AcquireTokenForClient(new[] { scope })
+            AuthenticationResult authResult = await daemonClient.AcquireTokenForClient(new[] { OnboardingScope })
                 .ExecuteAsync();
 
             return authResult;
