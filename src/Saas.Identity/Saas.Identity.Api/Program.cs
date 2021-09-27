10b8bc8e5fe42d945c7d8c613dd0c54e01ddfa2b
Nick Pinheiro <nickpinheiro@outlook.com>
2021-09-27 14:25:51 -0400

    Added initial Identity API.

---
+++ src/Saas.Identity/Saas.Identity.Api/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Saas.Identity.Api", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Saas.Identity.Api v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
