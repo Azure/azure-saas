972720d89c48ba06e4986210353b14e3cce1b6a4
Landon Pierce <landonpierce@microsoft.com>
2022-01-31 10:56:32 -0500

    create new project

---
+++ src/Saas.Catalog/Saas.Catalog.Api/Controllers/TenantsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Saas.Catalog.Api.Models;
+
+namespace Saas.Catalog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenantsController : ControllerBase
+    {
+        private readonly sqldbcatalogdevContext _context;
+
+        public TenantsController(sqldbcatalogdevContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tenants
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tenant>>> GetTenants()
+        {
+            return await _context.Tenants.ToListAsync();
+        }
+
+        // GET: api/Tenants/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tenant>> GetTenant(Guid id)
+        {
+            var tenant = await _context.Tenants.FindAsync(id);
+
+            if (tenant == null)
+            {
+                return NotFound();
+            }
+
+            return tenant;
+        }
+
+        // PUT: api/Tenants/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTenant(Guid id, Tenant tenant)
+        {
+            if (id != tenant.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tenant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TenantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tenants
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tenant>> PostTenant(Tenant tenant)
+        {
+            _context.Tenants.Add(tenant);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TenantExists(tenant.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTenant", new { id = tenant.Id }, tenant);
+        }
+
+        // DELETE: api/Tenants/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTenant(Guid id)
+        {
+            var tenant = await _context.Tenants.FindAsync(id);
+            if (tenant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tenants.Remove(tenant);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TenantExists(Guid id)
+        {
+            return _context.Tenants.Any(e => e.Id == id);
+        }
+    }
+}
--- src/Saas.Catalog/Saas.Catalog.Api/Startup.cs
+++ src/Saas.Catalog/Saas.Catalog.Api/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Saas.Catalog.Api.Models;
 
 namespace Saas.Catalog.Api
 {
@@ -32,17 +26,17 @@ namespace Saas.Catalog.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Saas.Catalog.Api", Version = "v1" });
             });
+
+            services.AddDbContext<sqldbcatalogdevContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("CatalogDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Saas.Catalog.Api v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Saas.Catalog.Api v1"));
 
             app.UseHttpsRedirection();
 
