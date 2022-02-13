18c6249eb2f08b567501425639d6c657041616be
asanjabi <asanjabi@users.noreply.github.com>
2022-02-12 21:39:51 -0800

    Fixed old tests

--- src/Saas.Catalog/Saas.Catalog.Api/Services/TenantService.cs
+++ src/Saas.Catalog/Saas.Catalog.Api/Services/TenantService.cs
@@ -4,6 +4,7 @@ using Saas.Domain.Exceptions;
 using Saas.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
@@ -20,54 +21,66 @@ namespace Saas.Catalog.Api.Services
 
         public async Task AddItemAsync(Tenant item)
         {
-            //_context.Tenants.Add(item);
+            CatalogTenant catalogTenant = CatalogTenant.FromTenant(item);
+
+            _context.Tenants.Add(catalogTenant);
             await _context.SaveChangesAsync();
+            item.Id = catalogTenant.Id;
         }
 
-        public async Task DeleteItemAsync(Guid Id)
+        public async Task DeleteItemAsync(Guid id)
         {
-            var tenant = await GetItemAsync(Id);
+            var tenant = await _context.Tenants.FindAsync(id);
             if (tenant == null)
             {
                 throw new TenantNotFoundException();
             }
             else
             {
-                //_context.Tenants.Remove(tenant);
+                _context.Tenants.Remove(tenant);
                 await _context.SaveChangesAsync();
             }
         }
 
         public async Task<Tenant> GetItemAsync(Guid id)
         {
-            //return await _context.Tenants.FindAsync(id);
-            throw new NotImplementedException();
+            CatalogTenant catalogTenant = await _context.Tenants.FindAsync(id);
+            
+            if(catalogTenant == null)
+            {
+                return null;
+            }
+
+            return CatalogTenant.ToTenant(catalogTenant);
         }
 
         public async Task<IEnumerable<Tenant>> GetItemsAsync()
         {
-            //return await _context.Tenants.ToListAsync();
-            throw new NotImplementedException();
+            IEnumerable<CatalogTenant> catalogTenants = await _context.Tenants.ToListAsync();
+
+            IEnumerable<Tenant> tenants = catalogTenants.Select(ct => CatalogTenant.ToTenant(ct));
+            return tenants;
         }
 
         public async Task UpdateItemAsync(Tenant item)
         {
-            throw new NotImplementedException();
 
-            //var tenant = await GetItemAsync(item.Id);
-            //if (tenant == null)
-            //{
-            //    throw new TenantNotFoundException();
-            //}
-            //tenant.Name = item.Name;
-            //tenant.UserId = item.UserId;
-            //tenant.IsActive = item.IsActive;
-            //tenant.IsCancelled = item.IsCancelled;  
-            //tenant.IsProvisioned = item.IsProvisioned;  
-            //tenant.ApiKey = item.ApiKey;
-            //tenant.CategoryId = item.CategoryId;
-            //_context.Tenants.Update(tenant);
-            //await _context.SaveChangesAsync();
+            var catalogTenant = await _context.Tenants.FindAsync(item.Id);
+            if (catalogTenant == null)
+            {
+                throw new TenantNotFoundException();
+            }
+            catalogTenant.Name = item.Name;
+            catalogTenant.UserId = item.UserId;
+            catalogTenant.IsActive = item.IsActive;
+            catalogTenant.IsCancelled = item.IsCancelled;
+            catalogTenant.IsProvisioned = item.IsProvisioned;
+            catalogTenant.ApiKey = item.ApiKey;
+            catalogTenant.CategoryId = item.CategoryId;
+
+            _context.Tenants.Update(catalogTenant);
+
+            await _context.SaveChangesAsync();
         }
 
     }
