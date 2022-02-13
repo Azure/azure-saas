1b5aa515f939bfb1e0d0d2682071a4b591f4db76
asanjabi <asanjabi@users.noreply.github.com>
2022-02-12 19:24:01 -0800

    Added test utiltities

---
+++ TestUtilities/Utility/LiveUnitTestUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace TestUtilities.Utility
+{
+    internal static class LiveUnitTestUtil
+    {
+        private static readonly bool SLiveUnitTestRuntimeLoaded = AppDomain.CurrentDomain.GetAssemblies()
+            .Any(a => a.GetName().Name == "Microsoft.CodeAnalysis.LiveUnitTesting.Runtime");
+
+        public static string? SkipIfLiveUnitTest(bool skip)
+        {
+            if (SLiveUnitTestRuntimeLoaded && skip)
+            {
+                return "Test excluded from Live Unit Testing";
+            }
+            return null;
+        }
+    }
+}
