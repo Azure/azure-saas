65a9f123b73610d399de7d8b76812b9b435f8827
beshaghy <beshaghy@gmail.com>
2022-02-15 09:26:07 -0800

    Revert "Creation of new Saas.LandingSingup solution to replace Saas.Provider. Updated all references to match the new namespace. Removed Saas.Provider from the larger solution"
    This reverts commit 98e954b047733cbf61d522cd4b47ff9abf987347.

--- src/Saas.LandingSignup/Saas.LandingSignup.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Saas.LandingSignup.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
