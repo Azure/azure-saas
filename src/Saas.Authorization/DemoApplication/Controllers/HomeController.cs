8c7fec65caf1c043f5e439025a6e03d4b0d8fe56
asanjabi <asanjabi@users.noreply.github.com>
2022-03-09 20:10:14 -0800

    Demo app before any changes

---
+++ src/Saas.Authorization/DemoApplication/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DemoApplication.Models;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
+namespace DemoApplication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
