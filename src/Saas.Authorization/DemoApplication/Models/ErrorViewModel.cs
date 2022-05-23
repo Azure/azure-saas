734a22591b861ccda8dbcccec3a373054424c16f
julian-mcnichols <julianmcnichols@gmail.com>
2022-05-23 09:41:07 -0500

    Remove Demo Project Deleted, No Remaining References Found

--- src/Saas.Authorization/DemoApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
