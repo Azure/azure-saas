2a9ae3227bec0363f0d4ca483b0ce9aa6495a771
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-11-17 12:54:08 -0800

    Create Cla repo.php

---
+++ Cla repo.php
@@ -0,0 +1,62 @@
+Contributor License Agreements
+We appreciate community contributions 
+to Microsoft repositories
+By signing a contributor license agreement
+we ensure that the community is free to use 
+yyour contributions
+Review the CLA document
+Download the Microsoft Contributor License Agreement 
+as a PDF document 
+if you need
+Signing the CLA
+The Microsoft CLA experience
+is integrated natively into GitHub 
+and its pull request experience Heres how it 
+works the first time you contribute to a Microsoft open source
+project: When you open a pull request the 
+license/
+cla check is run by our bot
+If you've signed the CLA before
+the status check goes green and youre done
+Your pull request is ready to be reviewed and 
+merged If you have not signed the 
+CLA at any point before the pull request 
+will receive a comment annotated with the agreement
+and more information about signing the aagreemen You will reply with a comment 
+to accept the agreement on behalf of yourself 
+or if youve been authorized to sign on behalf of your company there is a different 
+syntax for that
+After signing the status check will pass and 
+youre all set Thanks for your 
+contribution!
+NOTE:
+If you're an active Microsoft employee or 
+vendor
+there is a Microsoft-internal 
+system that allows you to 
+"link" your GitHub 
+account for corporate 
+use When you use your
+linked account you will not 
+have to sign the CLA. Microsoft employees and vendors can learn more about linking accounts at aka.ms/opensource.
+.NET Foundation CLA
+The 
+.NET Foundation is a 
+501(c)(6)
+non-profit organization which was established 
+to support an innovative commercially friendly, open-source ecosystem around the .NET platform.
+While Microsoft assists with infrastructure alignment, the foundation maintains a separate CLA. You can read more about this process at https://cla.dotnetfoundation.org/. Even Microsoft employees must sign the .NET Foundation CLA to cover their contributions to the community.
+Corporate CLA (CCLA)
+Microsoft does not typically participate in corporate
+CLA agreements given the scale and logistics challenge.
+However, we recognize that some companies
+especially those in highly regulated industries
+need to have the ability to explicitly manage
+specific people who have access to participate in Microsoft projects
+To enable these scenarios
+an authorized individual or group from such a company can receive a 
+special "CCLA repository" that will allow your firm to manually manage specific GitHub logins that are authorized on behalf of your company. You can also use GitHub Actions or other technology to customize and automate this list.
+A CCLA repository is a bespoke process.
+If you feel your company is a good fit for
+this please reach out to the Microsoft Open 
+Source Programs Office at <opensource@microsoft.com>
